22954a51560a89da12efacefad0e6f0b504015df
BrokenArrowIGU <danielmqssl@gmail.com>
2025-03-05 11:01:15 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioBanco.py
@@ -0,0 +1,58 @@
+
+# Definições Globais
+saldo = 0
+limite_s = 500
+n_saque = 0
+limite_diario = 3
+
+# Programa geral
+while True:
+    print( f""" 
+    [d] Depósito
+    [s] Saque
+    [e] Extrato
+    [q] Sair
+    """ )
+    opt = input("Escolha uma opção: ")
+    
+    # Depósito
+    if opt == "d":
+        saldo_p = float(input("Qual o valor do seu depósito? "))
+        if saldo_p <= 0:
+            print("Valor inválido, somente valores positivos, tente novamente.")
+        else:
+            saldo += saldo_p
+            print(f"Depósito de R${saldo_p:.2f} realizado com sucesso.")
+    
+    # Saque
+    elif opt == "s":
+        if saldo == 0:
+            print("Sem valor disponível para saque!")
+        elif n_saque >= limite_diario:
+            print("Limite diário de saques atingido, tente em data posterior.")
+        else:
+            saque = float(input("Qual o valor do saque? "))
+            if saque > limite_s:
+                print("Saque não permitido, valor acima do limite.")
+            elif saque > saldo:
+                print("Saque não permitido, saldo insuficiente.")
+            else:
+                saldo -= saque
+                n_saque += 1
+                print(f"Saque de R${saque:.2f} realizado com sucesso.")
+    
+    # Extrato
+    elif opt == "e":
+        print(f"""
+Extrato:
+Saques Diários: {n_saque}
+Saques Ainda Permitidos: {limite_diario - n_saque}
+Saldo Disponível: R${saldo:.2f}
+""")
+    
+    # Sair
+    elif opt == "q":
+        break
+    
+    else:
+        print("Operação inválida\nSelecione novamente a operação desejada.")
