9f9545cbd35673856ca044d78c53a6a9c685e650
BrokenArrowIGU <88664697+BrokenArrowIGU@users.noreply.github.com>
2025-03-07 13:44:57 -0300

    Rename DesafioBancov1.py to DesafioBancov0.py

--- DesafioBancov0.py
+++ DesafioBancov0.py
@@ -10,18 +10,18 @@ limite_diario = 3
 # Programa geral
 while True:
     print( f""" 
-    [d] Depósito
+    [d] DepÃ³sito
     [s] Saque
     [e] Extrato
     [q] Sair
     """ )
-    opt = input("Escolha uma opção: ")
+    opt = input("Escolha uma opÃ§Ã£o: ")
     
     # Deposito
     if opt == "d":
-        saldo_p = float(input("Qual o valor do seu depósito? "))
+        saldo_p = float(input("Qual o valor do seu depÃ³sito? "))
         if saldo_p <= 0:
-            print("Valor inválido, somente valores positivos, tente novamente.")
+            print("Valor invÃ¡lido, somente valores positivos, tente novamente.")
         else:
             saldo += saldo_p
             print(f"Deposito de R${saldo_p:.2f} realizado com sucesso.")
@@ -29,15 +29,15 @@ while True:
     # Saque
     elif opt == "s":
         if saldo == 0:
-            print("Sem valor disponível para saque!")
+            print("Sem valor disponÃ­vel para saque!")
         elif n_saque >= limite_diario:
             print("Limite diario de saques atingido, tente em data posterior.")
         else:
             saque = float(input("Qual o valor do saque? "))
             if saque > limite_s:
-                print("Saque não permitido, valor acima do limite.")
+                print("Saque nÃ£o permitido, valor acima do limite.")
             elif saque > saldo:
-                print("Saque não permitido, saldo insuficiente.")
+                print("Saque nÃ£o permitido, saldo insuficiente.")
             else:
                 saldo -= saque
                 n_saque += 1
@@ -47,9 +47,9 @@ while True:
     elif opt == "e":
         print(f"""
 Extrato:
-Saques Diários: {n_saque}
+Saques DiÃ¡rios: {n_saque}
 Saques Ainda Permitidos: {limite_diario - n_saque}
-Saldo Disponível: R${saldo:.2f}
+Saldo DisponÃ­vel: R${saldo:.2f}
 """)
     
     # Sair
@@ -57,5 +57,5 @@ Saldo Disponível: R${saldo:.2f}
         break
     
     else:
-        print("Operação inválida\nSelecione novamente a operação desejada.")
+        print("OperaÃ§Ã£o invÃ¡lida\nSelecione novamente a operaÃ§Ã£o desejada.")
 quit
